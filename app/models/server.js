e8938a5e52454b1f0ded2793dfd4c0305763d690
Josue Rivas <reycoc2311@gmail.com>
2022-06-28 16:40:02 -0600

    the last one v3

--- app/models/server.js
+++ app/models/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 
 /* libs */
-const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const { dbConnection } = require('../database/config');
 
